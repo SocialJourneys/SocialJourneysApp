263daa14b20ecfd100fc6b15e16deb179af95054
marki18 <markovicsk@yahoo.com>
2015-08-04 14:57:40 +0100

    minor fixes
    added code ensuring that the system looks for links between events and
    bus services based on presence of a street or bus name in the message
    only if link dont already exist as part of the annotation model returned
    by the KIM+SPIN service

--- src/dotrural/ac/uk/twitter_manager/EventsEffectsOnBusServicesChecker.java
+++ src/dotrural/ac/uk/twitter_manager/EventsEffectsOnBusServicesChecker.java
@@ -4,6 +4,8 @@ import com.hp.hpl.jena.query.DatasetAccessor;
 import com.hp.hpl.jena.query.DatasetAccessorFactory;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
 
 import dotrural.ac.uk.constants.PredefinedConstants;
 
@@ -14,13 +16,21 @@ public class EventsEffectsOnBusServicesChecker {
        
 		DatasetAccessor busServicesStoreAccessor = DatasetAccessorFactory.createHTTP(PredefinedConstants.FUSEKI_BUS_SERVICES_URI); 
 		
+		Property p = ResourceFactory.createProperty("http://vocab.org/transit/terms/service");
+		
+		boolean inferenecesBetweenBusServicesAndEventsExist = messageAnnotationsReturnedByKIMandInferences.containsLiteral(null, p, null);
+		
+		//if not inferences between bus services and events exist, try use the street names and bus stops
+		if (!inferenecesBetweenBusServicesAndEventsExist) {
+		
+		
 		
 		/// the code for handling the relations beteen busroutes and tweets goes here 
 		
 		Model newTriples =  ModelFactory.createDefaultModel();
 		 
 		mainEventsStorage.add(newTriples); 
-		
+		}
 		
 	}
 
